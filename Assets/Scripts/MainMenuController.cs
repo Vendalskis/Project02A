1ba0aa8e64e13a20e85885cc6dd6f927c140647a
Dawson <dawsonvendal@gmail.com>
2020-08-02 14:33:38 -0500

    Added audio script

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour {
+
+    [SerializeField] AudioClip _startingSong;
+
+    //Start is called before the first frame update
+    void Start()
+    {
+    if(_startingSong != null)
+        {
+            AudioManager.Instance.PlaySong(_startingSong);
+        }
+    }
+}
